0cad88d54a06716ab593f1fc901c11edfb3f923f
mp-keyner <keyneroswaldo0@gmail.com>
2023-04-28 17:09:47 -0500

    vamos

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
+const nav = document.querySelector('.nav');
+const modo = document.querySelector('.mod');
+const container = document.querySelector('.container');
+const containerSer = document.querySelector('.containerSer');
+const containerLogin = document.querySelector('.containerLogin');
+
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
   menu.classList.toggle('nav-active');
+});
+
+modo.addEventListener('click', () => {
+  nav.classList.toggle('nav-oscuro');
+  container.classList.toggle('container-oscuro');
+  containerSer.classList.toggle('containerSer-oscuro');
+  containerLogin.classList.toggle('containerLogin-oscuro');
 });
